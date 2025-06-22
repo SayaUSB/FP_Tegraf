ff61edb5eeff7cf278c0a260128bbba42ead56ff
SayaUSB <binasyahdiba@gmail.com>
2025-06-22 21:05:38 +0700

    fix : Obstacle registration

--- demo_UI.py
+++ demo_UI.py
@@ -190,6 +190,8 @@ if __name__ == "__main__":
                         )
                         
                         planner.start_planning(sorted_cps)
+                    if planner.env.simulator.active_mode == "Add Obstacle":
+                        planner.env.simulator.add_obstacle(np.array([event.pos[0], event.pos[1]], dtype=np.float32), 0.2)
         
         # Process state updates from planning thread
         while not planner.state_queue.empty():
